71cdcd2421cf0379ea73ebc1206c92987849b000
temitope david <temi236topemicheal@gmail.com>
2023-01-16 10:16:33 +0100

    feat:made the input in new-expense a controlled component

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,39 +1,87 @@
 import React from "react";
+import { useState } from "react";
 import { IoIosDoneAll, IoIosExit } from "react-icons/io";
 import "./ExpenseForm.modules.scss";
 
 function ExpenseForm() {
+  const [userinput, setUserinput] = useState({
+    enteredTitle: "",
+    enteredDate: "",
+    enteredAmount: "",
+  });
+
+  const titleHandler = (event) => {
+    setUserinput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+  const amountHandler = (event) => {
+    setUserinput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
+  };
+  const dateHandler = (event) => {
+    setUserinput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = userinput;
+    console.log(expenseData);
+    setUserinput({
+      enteredTitle: "",
+      enteredDate: "",
+      enteredAmount: "",
+    });
+  };
   return (
     <>
       <div className="expense-form">
-        <form action="#">
+        <form action="#" onSubmit={submitHandler}>
           <div className="expense-form__split">
             <div className="expense-form__control">
-              <label htmlFor="">name</label>
-              <input type="text" />
+              <label htmlFor="name">title</label>
+              <input
+                value={{ ...userinput }.enteredTitle}
+                type="text"
+                onChange={titleHandler}
+              />
             </div>
             <div className="expense-form__control expense-form__control--amt">
               <label htmlFor="">Amount</label>
-              <input type="text" />
+              <input
+                // value={(prevState) => {
+                //   return { ...prevState, enteredAmount: "" };
+                // }}
+                type="text"
+                onChange={amountHandler}
+              />
             </div>
           </div>
           <div className="expense-form__split  expense-form__split--second ">
             <div className="expense-form__control">
               <label htmlFor="name">date</label>
-              <input type="date" />
+              <input
+                // value={(prevState) => {
+                //   return { ...prevState, enteredDate: "" };
+                // }}
+                onChange={dateHandler}
+                type="date"
+              />
             </div>
-            {/* <button>cancel</button>
-          <button>add expense</button> */}
 
             <div className="buttons">
-              <button type="submit" class="uiverse">
-                <span class="tooltip">
+              <button type="reset" className="uiverse">
+                <span className="tooltip">
                   <IoIosExit />
                 </span>
                 <span>cancel</span>
               </button>
-              <button type="submit" class="uiverse">
-                <span class="tooltip">
+              <button type="submit" className="uiverse">
+                <span className="tooltip">
                   <IoIosDoneAll />
                 </span>
                 <span>add</span>
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -14,3 +14,4 @@ function NewExpense() {
 }
 
 export default NewExpense;
+  
